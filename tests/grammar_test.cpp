7ee1aad319c4a9a1bc22241ad75dc585e2656eef
Aleksander Tudruj <aleksanderwt@gmail.com>
2025-04-13 16:14:06 +0200

    test cleanup/refactor

--- tests/grammar_test.cpp
+++ tests/grammar_test.cpp
@@ -1,5 +1,92 @@
 #include <gtest/gtest.h>
 #include "grammar.h"
+#include "test_properties.hpp"
+
+void ASSERT_PRODUCTIONS_EQ(
+        const std::vector<Production> &productions,
+        const std::vector<Production> &expected_productions
+);
+
+void ASSERT_PRODUCTIONS_DATA_EQ(
+        const OpenGLReadyProductions &productions,
+        const std::vector<Production> &expected_productions
+);
+
+TEST(Grammar, GrammarCreation) {
+    auto grammar = load_grammar(FIXTURE_PATH("resources/all_basic_abc.ls"));
+
+    ASSERT_NE(grammar, nullptr);
+
+    ASSERT_EQ(grammar->get_property_float("step"), 10.0);
+    ASSERT_EQ(grammar->get_property_size_t("depth"), 20);
+    ASSERT_EQ(grammar->get_property_float("delta"), 90);
+
+    auto productions = grammar->get_raw_productions();
+    auto expected_productions = std::vector<Production>{
+            {'B', "AA", {}, {}, {0, 0}},
+            {'A', "BB", {}, {}, {0, 0}},
+            {'C', "A",  {}, {}, {0}},
+            {'C', "AA", {}, {}, {0, 0}}
+    };
+
+    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
+
+    auto opengl_data = grammar->get_opengl_ready_productions();
+    ASSERT_PRODUCTIONS_DATA_EQ(opengl_data, expected_productions);
+}
+
+TEST(Grammar, GrammarContextCheck) {
+    auto grammar = load_grammar(FIXTURE_PATH("resources/basic_context.ls"));
+
+    ASSERT_NE(grammar, nullptr);
+
+    ASSERT_EQ(grammar->get_property_float("step"), 10.0);
+    ASSERT_EQ(grammar->get_property_size_t("depth"), 20);
+    ASSERT_EQ(grammar->get_property_float("delta"), 45);
+
+    ASSERT_EQ(grammar->get_axiom()->axiom, "aAa");
+
+    auto productions = grammar->get_raw_productions();
+    auto expected_productions = std::vector<Production>{
+            {'A', "AA", "a", "a", {0, 0}},
+            {'A', "LA", "a", "",  {0, 0}},
+            {'A', "AL", "",  "a", {0, 0}},
+            {'L', "LL", "",  "",  {0, 0}},
+            {'L', "FF", "",  "",  {0, 0}}
+    };
+    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
+
+    auto opengl_data = grammar->get_opengl_ready_productions();
+    ASSERT_PRODUCTIONS_DATA_EQ(opengl_data, expected_productions);
+}
+
+TEST(Grammar, GrammarLookBackCheck) {
+    auto grammar = load_grammar(FIXTURE_PATH("resources/lookback_computation.ls"));
+
+    ASSERT_NE(grammar, nullptr);
+
+    ASSERT_EQ(grammar->get_property_float("step"), 100.0);
+    ASSERT_EQ(grammar->get_property_size_t("depth"), 7);
+    ASSERT_EQ(grammar->get_property_float("delta"), 22.5);
+
+    ASSERT_EQ(grammar->get_axiom()->axiom, "FP");
+
+    LookBackTable expected_axiom_lookback = {0, 0};
+    ASSERT_EQ(grammar->get_axiom()->look_back, expected_axiom_lookback);
+
+    auto productions = grammar->get_raw_productions();
+    auto expected_productions = std::vector<Production>{
+            {'P', "D[+P][-P][^P][&P]", "", "", {0, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3}},
+            {'D', "DF",                "", "", {0, 0}},
+            {'A', "{AfA}",             "", "", {4, 0, 0, 0, -4}},
+    };
+    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
+
+    auto opengl_data = grammar->get_opengl_ready_productions();
+    ASSERT_PRODUCTIONS_DATA_EQ(opengl_data, expected_productions);
+}
+
+/* UTILS */
 
 void ASSERT_PRODUCTIONS_EQ(
         const std::vector<Production> &productions,
@@ -23,176 +110,71 @@ std::vector<int32_t> string_to_int32_t_vector(const std::string &str) {
     return result;
 }
 
-TEST(Grammar, GrammarCreation) {
-    auto grammar = load_grammar("resources/all_basic_abc.ls");
-
-    ASSERT_NE(grammar, nullptr);
-
-    ASSERT_EQ(grammar->get_property_float("step"), 10.0);
-    ASSERT_EQ(grammar->get_property_size_t("depth"), 20);
-    ASSERT_EQ(grammar->get_property_float("delta"), 90);
-
-    auto productions = grammar->get_raw_productions();
-    auto expected_productions = std::vector<Production>{
-            {'B', "AA", {}, {}, {0, 0}},
-            {'A', "BB", {}, {}, {0, 0}},
-            {'C', "A",  {}, {}, {0}},
-            {'C', "AA", {}, {}, {0, 0}}
-    };
-
-    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
-
-    auto opengl_data = grammar->get_opengl_ready_productions();
-
-    std::vector<int32_t> expected_predecessors = string_to_int32_t_vector("BACC");
-    ASSERT_EQ(*opengl_data.predecessors, expected_predecessors);
-
-    std::vector<int32_t> expected_lookback = {0, 0, 0, 0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.look_back, expected_lookback);
-
-    std::vector<int32_t> expected_successors_sizes = {2, 2, 1, 2};
-    ASSERT_EQ(*opengl_data.successors_sizes, expected_successors_sizes);
-
-    std::vector<int32_t> expected_successors_offsets = {0, 2, 4, 5};
-    ASSERT_EQ(*opengl_data.successors_offsets, expected_successors_offsets);
-
-    std::vector<int32_t> expected_successors_data = string_to_int32_t_vector("AABBAAA");
-    ASSERT_EQ(*opengl_data.successors_data, expected_successors_data);
-
-    std::vector<int32_t> expected_left_context_sizes = {0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.left_context_sizes, expected_left_context_sizes);
-
-    std::vector<int32_t> expected_left_context_offsets = {0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.left_context_offsets, expected_left_context_offsets);
-
-    std::vector<int32_t> expected_left_context_data = {};
-    ASSERT_EQ(*opengl_data.left_context_data, expected_left_context_data);
-
-    std::vector<int32_t> expected_right_context_sizes = {0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.right_context_sizes, expected_right_context_sizes);
-
-    std::vector<int32_t> expected_right_context_offsets = {0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.right_context_offsets, expected_right_context_offsets);
-
-    std::vector<int32_t> expected_right_context_data = {};
-    ASSERT_EQ(*opengl_data.right_context_data, expected_right_context_data);
+std::vector<int32_t> calc_expected_predecessors(
+        const std::vector<Production> &productions
+) {
+    std::vector<int32_t> predecessors;
+    predecessors.reserve(productions.size());
+    for (const auto &prod: productions) {
+        predecessors.push_back(static_cast<int32_t>(prod.predecessor));
+    }
+    return predecessors;
 }
 
-TEST(Grammar, GrammarContextCheck) {
-    auto grammar = load_grammar("resources/basic_context.ls");
-
-    ASSERT_NE(grammar, nullptr);
-
-    ASSERT_EQ(grammar->get_property_float("step"), 10.0);
-    ASSERT_EQ(grammar->get_property_size_t("depth"), 20);
-    ASSERT_EQ(grammar->get_property_float("delta"), 45);
-
-    ASSERT_EQ(grammar->get_axiom()->axiom, "aAa");
-
-    auto productions = grammar->get_raw_productions();
-    auto expected_productions = std::vector<Production>{
-            {'A', "AA", "a", "a", {0, 0}},
-            {'A', "LA", "a", "",  {0, 0}},
-            {'A', "AL", "",  "a", {0, 0}},
-            {'L', "LL", "",  "",  {0, 0}},
-            {'L', "FF", "",  "",  {0, 0}}
-    };
-    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
-
-    auto opengl_data = grammar->get_opengl_ready_productions();
-
-    std::vector<int32_t> expected_predecessors = string_to_int32_t_vector("AAALL");
-    ASSERT_EQ(*opengl_data.predecessors, expected_predecessors);
-
-    std::vector<int32_t> expected_lookback = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.look_back, expected_lookback);
-
-    std::vector<int32_t> expected_successors_sizes = {2, 2, 2, 2, 2};
-    ASSERT_EQ(*opengl_data.successors_sizes, expected_successors_sizes);
-
-    std::vector<int32_t> expected_successors_offsets = {0, 2, 4, 6, 8};
-    ASSERT_EQ(*opengl_data.successors_offsets, expected_successors_offsets);
-
-    std::vector<int32_t> expected_successors_data = string_to_int32_t_vector("AALAALLLFF");
-    ASSERT_EQ(*opengl_data.successors_data, expected_successors_data);
-
-    std::vector<int32_t> expected_left_context_sizes = {1, 1, 0, 0, 0};
-    ASSERT_EQ(*opengl_data.left_context_sizes, expected_left_context_sizes);
-
-    std::vector<int32_t> expected_left_context_offsets = {0, 1, 2, 2, 2};
-    ASSERT_EQ(*opengl_data.left_context_offsets, expected_left_context_offsets);
-
-    std::vector<int32_t> expected_left_context_data = string_to_int32_t_vector("aa");
-    ASSERT_EQ(*opengl_data.left_context_data, expected_left_context_data);
-
-    std::vector<int32_t> expected_right_context_sizes = {1, 0, 1, 0, 0};
-    ASSERT_EQ(*opengl_data.right_context_sizes, expected_right_context_sizes);
-
-    std::vector<int32_t> expected_right_context_offsets = {0, 1, 1, 2, 2};
-    ASSERT_EQ(*opengl_data.right_context_offsets, expected_right_context_offsets);
-
-    std::vector<int32_t> expected_right_context_data = string_to_int32_t_vector("aa");
-    ASSERT_EQ(*opengl_data.right_context_data, expected_right_context_data);
+std::vector<int32_t> calc_expected_lookback(
+        const std::vector<Production> &productions
+) {
+    std::vector<int32_t> lookback_result;
+    for (const auto &prod: productions) {
+        auto &lookback = prod.look_back;
+        lookback_result.insert(lookback_result.end(), lookback.begin(), lookback.end());
+    }
+    return lookback_result;
 }
 
-TEST(Grammar, GrammarLookBackCheck) {
-    auto grammar = load_grammar("resources/lookback_computation.ls");
-
-    ASSERT_NE(grammar, nullptr);
-
-    ASSERT_EQ(grammar->get_property_float("step"), 100.0);
-    ASSERT_EQ(grammar->get_property_size_t("depth"), 7);
-    ASSERT_EQ(grammar->get_property_float("delta"), 22.5);
-
-    ASSERT_EQ(grammar->get_axiom()->axiom, "FP");
-
-    LookBackTable expected_axiom_lookback = {0, 0};
-    ASSERT_EQ(grammar->get_axiom()->look_back, expected_axiom_lookback);
-
-    auto productions = grammar->get_raw_productions();
-    auto expected_productions = std::vector<Production>{
-            {'P', "D[+P][-P][^P][&P]", "", "", {0, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3}},
-            {'D', "DF",                "", "", {0, 0}},
-            {'A', "{AfA}",             "", "", {4, 0, 0, 0, -4}},
-    };
-    ASSERT_PRODUCTIONS_EQ(productions, expected_productions);
-
-    auto opengl_data = grammar->get_opengl_ready_productions();
-
-    std::vector<int32_t> expected_predecessors = string_to_int32_t_vector("PDA");
-    ASSERT_EQ(*opengl_data.predecessors, expected_predecessors);
-
-    std::vector<int32_t> expected_lookback = {
-            0, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3, 3, 0, 0, -3,
-            0, 0,
-            4, 0, 0, 0, -4
-    };
-    ASSERT_EQ(*opengl_data.look_back, expected_lookback);
-
-    std::vector<int32_t> expected_successors_sizes = {17, 2, 5};
-    ASSERT_EQ(*opengl_data.successors_sizes, expected_successors_sizes);
-
-    std::vector<int32_t> expected_successors_offsets = {0, 17, 19};
-    ASSERT_EQ(*opengl_data.successors_offsets, expected_successors_offsets);
-
-    std::vector<int32_t> expected_successors_data = string_to_int32_t_vector("D[+P][-P][^P][&P]DF{AfA}");
-    ASSERT_EQ(*opengl_data.successors_data, expected_successors_data);
-
-    std::vector<int32_t> expected_left_context_sizes = {0, 0, 0};
-    ASSERT_EQ(*opengl_data.left_context_sizes, expected_left_context_sizes);
-
-    std::vector<int32_t> expected_left_context_offsets = {0, 0, 0};
-    ASSERT_EQ(*opengl_data.left_context_offsets, expected_left_context_offsets);
-
-    std::vector<int32_t> expected_left_context_data = {};
-    ASSERT_EQ(*opengl_data.left_context_data, expected_left_context_data);
-
-    std::vector<int32_t> expected_right_context_sizes = {0, 0, 0};
-    ASSERT_EQ(*opengl_data.right_context_sizes, expected_right_context_sizes);
-
-    std::vector<int32_t> expected_right_context_offsets = {0, 0, 0};
-    ASSERT_EQ(*opengl_data.right_context_offsets, expected_right_context_offsets);
-
-    std::vector<int32_t> expected_right_context_data = {};
-    ASSERT_EQ(*opengl_data.right_context_data, expected_right_context_data);
+std::tuple<std::vector<int32_t>, std::vector<int32_t>, std::vector<int32_t>> calc_expected_triple(
+        const std::vector<Production> &productions,
+        const std::function<const std::string *(const Production &)> &base
+) {
+    std::vector<int32_t> sizes;
+    sizes.reserve(productions.size());
+    std::vector<int32_t> offsets;
+    offsets.reserve(productions.size());
+    std::vector<int32_t> data;
+    for (const auto &prod: productions) {
+        auto prop = base(prod);
+        auto size = static_cast<int32_t>(prop->size());
+        sizes.push_back(size);
+        offsets.push_back(static_cast<int32_t>(data.size()));
+        auto prop_casted = string_to_int32_t_vector(*prop);
+        data.insert(data.end(), prop_casted.begin(), prop_casted.end());
+    }
+    return {sizes, offsets, data};
 }
+
+void ASSERT_PRODUCTIONS_DATA_EQ(
+        const OpenGLReadyProductions &productions,
+        const std::vector<Production> &expected_productions
+) {
+    auto expected_predecessors = calc_expected_predecessors(expected_productions);
+    auto expected_lookback = calc_expected_lookback(expected_productions);
+    auto [expected_successors_sizes, expected_successors_offsets, expected_successors_data] =
+            calc_expected_triple(expected_productions, [](const Production &p) { return &p.successor; });
+    auto [expected_left_context_sizes, expected_left_context_offsets, expected_left_context_data] =
+            calc_expected_triple(expected_productions, [](const Production &p) { return &p.left_context; });
+    auto [expected_right_context_sizes, expected_right_context_offsets, expected_right_context_data] =
+            calc_expected_triple(expected_productions, [](const Production &p) { return &p.right_context; });
+
+    ASSERT_EQ(*productions.predecessors, expected_predecessors);
+    ASSERT_EQ(*productions.look_back, expected_lookback);
+    ASSERT_EQ(*productions.successors_sizes, expected_successors_sizes);
+    ASSERT_EQ(*productions.successors_offsets, expected_successors_offsets);
+    ASSERT_EQ(*productions.successors_data, expected_successors_data);
+    ASSERT_EQ(*productions.left_context_sizes, expected_left_context_sizes);
+    ASSERT_EQ(*productions.left_context_offsets, expected_left_context_offsets);
+    ASSERT_EQ(*productions.left_context_data, expected_left_context_data);
+    ASSERT_EQ(*productions.right_context_sizes, expected_right_context_sizes);
+    ASSERT_EQ(*productions.right_context_offsets, expected_right_context_offsets);
+    ASSERT_EQ(*productions.right_context_data, expected_right_context_data);
+}
+
